bb877a4b87cca3c786969a712a83caa238fb0888
VoidG4 <gregorytomazos@outlook.com>
2025-03-29 21:37:47 +0200

    menu panes added

--- src/main/java/com/example/gbsproject/HomePageViewController.java
+++ src/main/java/com/example/gbsproject/HomePageViewController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 
 import java.io.IOException;
@@ -22,6 +23,84 @@ public class HomePageViewController {
     public Button buttonMenuAccount;
     public AnchorPane mainAnchorPane;
     public Button buttonMenuStudies;
+    @FXML
+    private AnchorPane accountPane;
+    @FXML
+    private AnchorPane helpPane;
+    @FXML
+    private AnchorPane studiesPane;
+
+    @FXML
+    protected void formClicked() {
+        accountPane.setVisible(false);
+        helpPane.setVisible(false);
+        studiesPane.setVisible(false);
+    }
+
+
+    @FXML
+    protected void accountClicked() {
+        if(accountPane.isVisible()){
+            accountPane.setVisible(false);
+        } else {
+            accountPane.setVisible(true);
+            helpPane.setVisible(false);
+            studiesPane.setVisible(false);
+        }
+    }
+
+    @FXML
+    protected void studiesClicked() {
+        if(studiesPane.isVisible()){
+            studiesPane.setVisible(false);
+        } else {
+            studiesPane.setVisible(true);
+            helpPane.setVisible(false);
+            accountPane.setVisible(false);
+        }
+    }
+
+    @FXML
+    protected void helpClicked() {
+        if(helpPane.isVisible()){
+            helpPane.setVisible(false);
+        } else {
+            helpPane.setVisible(true);
+            accountPane.setVisible(false);
+            studiesPane.setVisible(false);
+        }
+    }
+
+    @FXML
+    protected void logOut() {
+        try {
+            // Load the FXML file for the login page
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login-page-view.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage (window) from the list of all windows
+            Stage stage = (Stage) Stage.getWindows().stream()
+                    .filter(Window::isShowing)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("No window found"));
+
+            double width = stage.getWidth();
+            double height = stage.getHeight();
+
+            // Set the new scene with the same window size
+            Scene scene = new Scene(root, width, height);
+            stage.setScene(scene);
+            stage.setWidth(width);
+            stage.setHeight(height);
+            stage.show();
+        } catch (IOException e) {
+            // Log the exception using a logger instead of printStackTrace()
+            LOGGER.log(Level.SEVERE, "An error occurred while loading the login page", e);
+            // Optionally, show a dialog to notify the user of the error
+            showErrorDialog();
+        }
+    }
+
 
     @FXML
     protected  void homeButtonClick(ActionEvent event) {
