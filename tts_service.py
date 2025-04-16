1ccf81408283d59027db503a62cc2167a8a7074d
VoidG4 <gregorytomazos@outlook.com>
2025-04-16 21:35:32 +0300

    history added

---
+++ tts_service.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+import os
+import sys
+
+# Get text from command-line argument
+text = sys.argv[1]
+
+# Create TTS object with British English (could try `en` for default)
+tts = gTTS(text=text, lang='en', tld='co.uk')
+
+# Save the speech as an MP3 file
+tts.save("speech.mp3")
+
+# Play the file (Linux-specific command)
+
